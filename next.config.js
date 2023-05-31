b3015f7f6ee9140f1934588f2508109c7bfaabbb
Awinash Kumar Jaiswal <awinashjaiswal100@gmail.com>
2023-05-31 20:21:39 +0530

    rewrite rule

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// rewrite rules should be removed on publishing
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ivrfootage.com/api/:path*',
+      },
+    ];
+  },
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
