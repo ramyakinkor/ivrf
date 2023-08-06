2a81971dc003638ed84418b12837b6b24e408a62
Awinash Kumar Jaiswal <awinashjaiswal100@gmail.com>
2023-08-07 00:05:48 +0530

    fix issues

--- src/components/Product/VideoRender.jsx
+++ src/components/Product/VideoRender.jsx
@@ -4,68 +4,108 @@ import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { specificItem } from "../../store/reducers/productSlice";
 import Pagination from "../common/Pagination";
+import HoverVideoPlayer from "react-hover-video-player";
 
-const FeaturedVideoComponent = ({product}) => {
-  const router = useRouter()
+const FeaturedVideoComponent = ({ product }) => {
+  const router = useRouter();
   const dispatch = useDispatch();
   function selectAndRoute(product) {
     dispatch(specificItem(product));
     router.push(`/product-details/video/${product.id}`);
   }
-  const ref = useRef();
   return (
     <div onClick={() => selectAndRoute(product)}>
       <div
-        onMouseEnter={() => ref.current.play()}
-        onMouseOut={() => ref.current.pause()}
-        className="inner_wrapper"
+        
       >
-        <video ref={ref}   muted>
-          <source src={product.public} />
-        </video>
-        <p className="render_title">{product.title}</p>
-        <p className="render_title" style={{bottom: "0.5em", top: "unset"}}>{product.resolution}</p>
+        <HoverVideoPlayer
+          videoSrc={product.public}
+          pausedOverlay={<PausedOverlay url={product.public.replace('public_assets', 'thumbnail').replace('mp4', 'jpeg')} />}
+          loadingOverlay={<Spinner />}
+          hoverOverlay={
+            <>
+              <p className="render_title" style={{color: 'white'}}>{product.title}</p>
+              <p className="render_title" style={{ position: "absolute", bottom: "0", top: "unset", color: 'white'}}>
+                {product.resolution}
+              </p>
+              
+            </>
+          }
+          preload="metadata"
+        />
       </div>
     </div>
   );
 };
-export default function VideoRender({videos}) {
+export default function VideoRender({ videos }) {
   const productsPerPage = 8;
   const [selectedPage, setSelectedPage] = useState(1);
-  const [videosOnCurrentPage, setVideosOnCurrentPage] = useState([])
+  const [videosOnCurrentPage, setVideosOnCurrentPage] = useState([]);
 
   useEffect(() => {
-    const {startIndex, endIndex} = getStartAndEndIndexByPageNumber(selectedPage, productsPerPage);
+    const { startIndex, endIndex } = getStartAndEndIndexByPageNumber(
+      selectedPage,
+      productsPerPage
+    );
     const selectedVideos = videos.slice(startIndex, endIndex);
     setVideosOnCurrentPage(selectedVideos);
   }, [videos]);
 
   function onPaginate(number) {
     setSelectedPage(number);
-    const {startIndex, endIndex} = getStartAndEndIndexByPageNumber(number, productsPerPage);
+    const { startIndex, endIndex } = getStartAndEndIndexByPageNumber(
+      number,
+      productsPerPage
+    );
     const selectedVideos = videos.slice(startIndex, endIndex);
-    console.log(selectedVideos)
+    console.log(selectedVideos);
     setVideosOnCurrentPage(selectedVideos);
   }
 
   function getStartAndEndIndexByPageNumber(pageNumber, itemsPerPage) {
-    const startIndex = (pageNumber * itemsPerPage) - itemsPerPage;
-    const endIndex = (pageNumber * itemsPerPage);
-    return {startIndex, endIndex}
+    const startIndex = pageNumber * itemsPerPage - itemsPerPage;
+    const endIndex = pageNumber * itemsPerPage;
+    return { startIndex, endIndex };
   }
   return (
     <>
-    <div className="video_wrapper">
-      {videosOnCurrentPage?.map((vid) => (
-        <FeaturedVideoComponent key={vid.id} product={vid} />
-      ))}
-    </div>
-    {videos.length > productsPerPage &&
-      <Pagination style={{display: 'flex', flexDirection: 'row-reverse'}}
-        totalPages={ Math.ceil(videos.length/productsPerPage)}
-        paginate={onPaginate} 
-        currentPage={selectedPage} />
-      }
-      </>
+      <div className="video_wrapper">
+        {videosOnCurrentPage?.map((vid) => (
+          <FeaturedVideoComponent key={vid.id} product={vid} />
+        ))}
+      </div>
+      {videos.length > productsPerPage && (
+        <Pagination
+          style={{ display: "flex", flexDirection: "row-reverse" }}
+          totalPages={Math.ceil(videos.length / productsPerPage)}
+          paginate={onPaginate}
+          currentPage={selectedPage}
+        />
+      )}
+    </>
   );
+}
+
+
+const PausedOverlay = ({url}) => (
+  <div>
+    <img
+      src={url}
+      alt=""
+      style={{
+        position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          objectFit: "cover"
+      }}
+      />
+  </div>
+);
+
+function Spinner() {
+  return (
+    <>Loading...</>
+  )
 }
